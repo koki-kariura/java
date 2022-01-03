6bdefa4e43eeef1ae294f46144652742cff475be
koki <kari_kou@icloud.com>
2022-01-03 21:10:29 +0900

    second commit

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,26 @@
 public class FizzBuzz {
 
     public static void main(String[] args) {
-        for(int i = 1; i <= 10; i++)
-        System.out.println(i);
+        
+        int i = 0;
+        for( i = 1; i <= Integer.parseInt(args[0]); i++) {
+        
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+        
+            else  { 
+                System.out.println(i); 
+            }
+            
+        }
     }
 }
