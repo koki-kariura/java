6cc216e58c77e8693c2a14e5bbd9d0b1c91b7cea
koki <kari_kou@icloud.com>
2022-01-04 11:05:54 +0900

    bunnpou1 kadai2

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,25 +2,29 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
         
-        int i = 0;
-        for( i = 1; i <= Integer.parseInt(args[0]); i++) {
-        
+        for(int i = 1; i <= Integer.parseInt(args[0]); i++) {
+        //コマンドライン引数に入力した数値以下まで１ずつ足していく
+           
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+                System.out.print("FizzBuzz");
             }
-
+            //３で割り切れてかつ５で割り切れる数値はFizzBuzzと出力する
+            
             else if (i % 5 == 0) {
-                System.out.println("Buzz");
+                System.out.print("Buzz");
             }
-            
+            //５でわりきれる数値はBuzzと出力する
+
             else if (i % 3 == 0) {
-                System.out.println("Fizz");
+                System.out.print("Fizz");
             }
-        
+            //３でわりきれる数値はFizzと出力する
+
             else  { 
-                System.out.println(i); 
+                System.out.print(i); 
             }
-            
+            //それ以外の数値を出力する
+                System.out.print(" ");
         }
     }
 }
